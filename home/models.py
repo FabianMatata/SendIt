d8a443aa53be18dc0d533955cb6a7fc68adf906b
feLycks <felix.obiero@student.moringaschool.com>
2022-04-07 18:15:23 +0300

    edit Created user model

--- home/models.py
+++ home/models.py
@@ -31,6 +31,6 @@ class newdestination(models.Model):
         return self
 
 class Cost(models.Model):
-    from_location = models.CharField(max_length=255, default='')
-    to_location = models.CharField(max_length=255, default='')
+    from_location = models.CharField(max_length=255, default='from-location')
+    to_location = models.CharField(max_length=255, default='to-location')
     price = models.IntegerField()
